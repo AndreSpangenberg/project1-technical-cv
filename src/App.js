56a17b32f7d201bd02d2ffcdf6564506c0239b74
AndreSpangenberg <spangenberg6227@gmail.com>
2020-10-07 21:51:37 +0200

    added code to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import NavigateBar from './components/NavigateBar';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import ContactInfo from './components/pages/ContactInfo';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Router>
+        <NavigateBar />
+        <Switch>
+          <Route path="/home" exact component = {Home} />
+          <Route path="/about" component = {About} />
+          <Route path="/contactInfo" component = {ContactInfo} />
+        </Switch>
+      </Router>
+    
+    </>
   );
 }
 
