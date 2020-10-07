e0aac85075b1b9195fc2bd115a3bf76a2363bbcc
AndreSpangenberg <spangenberg6227@gmail.com>
2020-10-07 21:55:27 +0200

    Added code to all pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+import Resume from '../resume';
+
+function About() {
+    return (
+      <>
+        <Resume />
+      </>
+    );
+}
+export default About;
